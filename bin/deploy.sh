5f37e17f329b9e1974fcc1e9f6851998cb55e1cb
wxianfeng <wang.fl1429@gmail.com>
2020-07-05 01:00:44 +0800

    bash 添加颜色

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,12 +1,12 @@
-echo "generate static files\n"
+echo "\033[32mgenerate static files\033[0m"
 hugo --theme=KeepIt --baseUrl="https://wxianfeng.github.io/"
-echo "generate static files DONE\n"
+echo "\033[32mgenerate static files DONE\033[0m"
 
-echo "static files push to github\n"
+echo "\033[32mstatic files push to github\033[0m"
 cd public && git add . && git commit -m "update wxianfeng.com content" && git push
-echo "static files push to github DONE\n"
+echo "\033[32mstatic files push to github DONE\033[0m"
 
-echo "source code push to github\n"
-cd ..
-git add . && git commit -m "update wxianfeng.com source code" && git push
-echo "source code push to github DONE\n"
+# echo "\033[32msource code push to github\033[0m"
+# cd ..
+# git add . && git commit -m "update wxianfeng.com source code" && git push
+# echo "\033[32msource code push to github DONE\033[0m"
